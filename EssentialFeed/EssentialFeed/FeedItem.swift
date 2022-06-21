6fc8c46381f771914057ad4197c67d5ade6d1aab
charleschen04627 <coge04627@gmail.com>
2022-06-21 21:37:53 +0800

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by 陳明辰 on 2022/6/21.
+//
+
+import Foundation
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
