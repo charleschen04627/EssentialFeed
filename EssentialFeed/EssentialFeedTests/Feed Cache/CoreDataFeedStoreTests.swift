22839fce1bfc9ca4270efc7778d82fcff5d95220
charleschen04627 <coge04627@gmail.com>
2022-09-08 10:29:34 +0800

    `CoreDataFeedStore.retrieve()` delivers found values on non-empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -21,7 +21,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_retrieve_deliversFoundvaluesOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNonSideEffectsOnNonEmptyCache() {
