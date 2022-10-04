8557a6502bcef5408daeadf319e16f8bbb09b073
charleschen04627 <coge04627@gmail.com>
2022-10-04 21:20:51 +0800

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by 陳明辰 on 2022/10/4.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by 陳明辰 on 2022/10/4.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
