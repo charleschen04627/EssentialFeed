b6833c59b1585742f4928d8fe8fb526f1a0da1fb
charleschen04627 <coge04627@gmail.com>
2022-10-21 21:48:14 +0800

    Move `CoreDataFeedStore` implementation of the `FeedImageDataStore` protocol to production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImagDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataFeedStore+FeedImagDataLoader.swift
+//  EssentialFeed
+//
+//  Created by 陳明辰 on 2022/10/21.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
