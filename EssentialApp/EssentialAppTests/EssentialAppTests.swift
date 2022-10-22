bfad4fcde8949370d6851514d36ad86f5e060f61
charleschen04627 <coge04627@gmail.com>
2022-10-22 14:15:43 +0800

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by 陳明辰 on 2022/10/22.
+//
+
+import XCTest
+@testable import EssentialApp
+
+final class EssentialAppTests: XCTestCase {}
